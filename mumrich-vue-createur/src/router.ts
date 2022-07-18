dc5afd3cf2271e128d6e1075dad2129503292141
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-07-18 18:54:24 +0200

    observe routes.

--- mumrich-vue-createur/src/router.ts
+++ mumrich-vue-createur/src/router.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import HomeVue from "./routes/Home.vue";
 import AboutVue from "./routes/About.vue";
+import { ref, markRaw } from "vue";
 
-export const routes: RouteRecordRaw[] = [
-  { path: "/", component: HomeVue, name: "Home" },
-  { path: "/about", component: AboutVue, name: "About" },
-];
+export const routes = ref([
+  { path: "/", component: markRaw(HomeVue), name: "Home" },
+  { path: "/about", component: markRaw(AboutVue), name: "About" },
+] as RouteRecordRaw[]);
 
 export const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes: routes.value,
 });

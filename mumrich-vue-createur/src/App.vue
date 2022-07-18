7f5eb3aa8a961b5048d901bafb17d366005e7d7f
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-07-18 18:40:03 +0200

    add icons.

--- mumrich-vue-createur/src/App.vue
+++ mumrich-vue-createur/src/App.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import LogosVue from "~icons/logos/vue";
+</script>
 
 <template>
-  <h1>App</h1>
+  <h1><LogosVue />App</h1>
 </template>
--- mumrich-vue-createur/vite.config.ts
+++ mumrich-vue-createur/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
 import WindiCSS from "vite-plugin-windicss";
+import Icons from "unplugin-icons/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+    }),
+    Icons({ compiler: "vue3" }),
+    WindiCSS(),
+  ],
 });

dd50ce7e83cb7b813535d20d8214b2f1b99d4258
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-07-22 14:52:35 +0200

    importCreateurWidgetsPromise.

--- mumrich-vue-createur/src/helpers/WidgetHelper.ts
+++ mumrich-vue-createur/src/helpers/WidgetHelper.ts
@@ -4,28 +4,49 @@ export const CREATEUR_WIDGETS_SEARCH_EXPR =
   /[a-zA-Z0-9\-_]+(?=\/([w|W]idget|[e|E]ditor|[t|T]emplate)\.vue)/;
 
 export interface CreateurWidget {
-  uid: string;
-  editor: Component;
-  template: Component;
-  widget: Component;
+  id: string;
+  editor?: () => Promise<Component>;
+  template?: () => Promise<Component>;
+  widget?: () => Promise<Component>;
   defaultProps: Record<string, any>;
 }
 
-export function importCreateurWidgetsPromise(): Promise<CreateurWidget[]> {
+export function importCreateurWidgetsPromise(): Promise<
+  Map<string, CreateurWidget>
+> {
   return new Promise((resolve, reject) => {
-    const createurWidgets: CreateurWidget[] = [];
-    const modules = import.meta.glob("../widgets/**/*.vue");
+    const modules = import.meta.glob<Component>("../widgets/**/*.vue");
+    const widgetsMap = new Map<string, CreateurWidget>();
 
-    Object.keys(modules)
-      .map((mod) => mod.match(CREATEUR_WIDGETS_SEARCH_EXPR))
-      .forEach((match) => console.log(match));
+    Object.keys(modules).forEach((mod) => {
+      const matches = mod.match(CREATEUR_WIDGETS_SEARCH_EXPR);
 
-    // for (const path in modules) {
-    //   modules[path]().then((mod) => {
-    //     console.log(path, mod);
-    //   });
-    // }
+      if (matches !== null && matches.length > 1 && matches.input) {
+        const widgetName = matches[0];
+        const componentType = matches[0];
+        const componentUri = matches.input;
 
-    resolve(createurWidgets);
+        if (!widgetsMap.has(widgetName)) {
+          widgetsMap.set(widgetName, {
+            id: widgetName,
+            [componentType]: modules[componentUri],
+            defaultProps: {},
+          });
+        } else {
+          const createurWidget = widgetsMap.get(widgetName) as CreateurWidget;
+          const newCreateurWidget: CreateurWidget = {
+            ...createurWidget,
+            [componentType]: modules[componentUri],
+          };
+          widgetsMap.set(widgetName, newCreateurWidget);
+        }
+      }
+    });
+
+    if (widgetsMap.size > 0) {
+      resolve(widgetsMap);
+    } else {
+      reject(new Error("no components found"));
+    }
   });
 }

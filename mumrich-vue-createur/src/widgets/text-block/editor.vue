610f3e6b9a217ba2e785e77b1682353a0e54f24b
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-07-30 16:14:17 +0200

    add quill-editor.

--- mumrich-vue-createur/src/widgets/text-block/editor.vue
+++ mumrich-vue-createur/src/widgets/text-block/editor.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>w-text-block: editor</h1>
+  <QuillEditor :options="qullOptions" />
 </template>
+
+<script setup lang="ts">
+  import { QuillEditor } from "@vueup/vue-quill";
+  import "@vueup/vue-quill/dist/vue-quill.snow.css";
+  import { ref } from "vue";
+
+  interface StringMap {
+    [key: string]: any;
+  }
+
+  interface QuillOptionsStatic {
+    debug?: string | boolean | undefined;
+    modules?: StringMap | undefined;
+    placeholder?: string | undefined;
+    readOnly?: boolean | undefined;
+    theme?: string | undefined;
+    formats?: string[] | undefined;
+    bounds?: HTMLElement | string | undefined;
+    scrollingContainer?: HTMLElement | string | undefined;
+    strict?: boolean | undefined;
+  }
+
+  const qullOptions = ref<QuillOptionsStatic>({
+    placeholder: "Compose an epic...",
+  });
+</script>

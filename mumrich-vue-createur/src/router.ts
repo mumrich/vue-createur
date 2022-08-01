ee5a2cb2b5d2bd22c45ca17eebe973a0d478f135
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-08-01 05:18:03 +0200

    add new route.

--- mumrich-vue-createur/src/router.ts
+++ mumrich-vue-createur/src/router.ts
@@ -3,15 +3,21 @@ import HomeVue from "./routes/Home.vue";
 import AboutVue from "./routes/About.vue";
 import { ref, markRaw } from "vue";
 import WidgetsSourceTemplateVue from "./routes/WidgetsSourceTemplate.vue";
+import RouterEditorVue from "./routes/RouterEditor.vue";
 
 export const routes = ref([
   { path: "/", component: markRaw(HomeVue), name: "Home" },
-  { path: "/about", component: markRaw(AboutVue), name: "About" },
   {
     path: "/widgets-source-template",
     component: markRaw(WidgetsSourceTemplateVue),
     name: "Widgets Source Template",
   },
+  {
+    path: "/router-editor",
+    component: markRaw(RouterEditorVue),
+    name: "Router Editor",
+  },
+  { path: "/about", component: markRaw(AboutVue), name: "About" },
 ] as RouteRecordRaw[]);
 
 export const router = createRouter({
---
+++ mumrich-vue-createur/src/routes/RouterEditor.vue
@@ -0,0 +1,1 @@
+<template>Router Editor</template>

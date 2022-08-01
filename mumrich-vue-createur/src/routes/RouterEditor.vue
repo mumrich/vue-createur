cc84ea686d72852567472503569af5625e90c771
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-08-01 05:26:45 +0200

    use full height, router-editor wip.

--- mumrich-vue-createur/src/routes/RouterEditor.vue
+++ mumrich-vue-createur/src/routes/RouterEditor.vue
@@ -1,1 +1,17 @@
-<template>Router Editor</template>
+<template>
+  <div class="wrapper">
+    <div>left</div>
+    <div>right</div>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    @apply w-full;
+    @apply flex flex-row justify-around;
+  }
+
+  .wrapper > * {
+    @apply bg-gray-200;
+  }
+</style>
